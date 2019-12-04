73dea34016496926efd620f5c6510f9773b4bc4c
peninah-njeri <peninahgathuru@gmail.com>
2019-12-04 12:18:16 +0300

    install bootstrap3 to ease styling

--- Recipe_project/settings.py
+++ Recipe_project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recipes.RecipesConfig',
+    'recipes.apps.RecipesConfig',
+    'bootstrap3'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
