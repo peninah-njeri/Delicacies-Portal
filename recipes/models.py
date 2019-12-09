739b1fe87d0d0d12f32412c541d8ba3defd6469d
peninah-njeri <peninahgathuru@gmail.com>
2019-12-09 12:42:23 +0300

    add two models to the recipe app. Model recipe and model ingredients

--- recipes/models.py
+++ recipes/models.py
@@ -3,3 +3,16 @@ from django.db import models
 # Create your model
 
 
+class Recipe(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=200),
+    description = models.TextField()
+
+
+class Ingredient(models.Model):
+    ingredients = models.TextField(null=False)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+
+
